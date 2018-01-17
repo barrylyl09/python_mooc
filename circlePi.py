bb344baa34df450df3b935b48518db888ea940d2
barry <barrylyl09@gmail.com>
2018-01-17 22:38:30 +0800

    update

--- circlePi.py
+++ circlePi.py
@@ -4,6 +4,13 @@
 import math
 import random
 
+'''
+蒙特卡洛（Monte Carlo）方法,又称随机抽样或统计试验方法。当所求解的问题是某种事件出现的概率，或某随机变量的期望值时，可以通过某种“试验”的方法求解。
+
+简单说，蒙特卡洛是利用随机试验求解问题的方法。
+
+
+'''
 
 from random import random
 from math import sqrt
