be782358a06c74448e3b173571afe8667de472af
LYL Barry <barrylyl09@gmail.com>
2018-01-17 17:53:59 +0800

    习题解答

---
+++ Python 学习手册.py
@@ -0,0 +1,65 @@
+# Python学习手册_第四版 (课后习题)
+
+'''
+
+第一章：
+
+1. 人们选择 Python 的六个主要原因是什么？
+
+答：软件质量、开发者效率、程序的可移植性、标准库的支持、组件集成和享受简便其中，质量和效率这两条是人们选择 Python 的主要原因。
+
+
+2. 请列举如今正在使用 Python 的四个著名的公司和组织的名称。
+
+答：Industrial Light & Magic、 EVE Online、 Jet Propulsion Labs、 Maya 和 ESRI 等。
+做软件开发的所有组织几乎都流行使用 Python，无论是长期战略产品开发还是测试或系统管理这一的短期策略任务都广泛采用了 Python。
+
+
+3. 出于什么样的原因会让你在应用中不使用 Python？
+
+答：Python 的缺点是它的性能：它不像 C 和 C++ 这类常规的编译语言运行得那么快。另一方面，它对于绝大多数应用已经足够快了，并且典型的
+Python 代码运行起来速度接近 C，因为在 Python 解释器中调用链接了 C 代码。如果速度要求很苛刻的话，应用的数值处理部分可以采用编译好
+的扩展以满足应用要求。
+
+
+4. 你可以用 Python 做什么？
+
+答：你几乎可以在计算机上的任何方面使用 Python：从网站和游戏开发到机器人和航天飞机控制。
+
+
+5. 在 Python 中 import this 有什么意义？
+
+答：import this 会出发 Python 内部的一个彩蛋，它将显示 Python 语言层面之下的设计哲学。
+
+
+'''
+
+'''
+第二章：
+
+1. 什么是 Python 解释器？
+
+答：Python 解释器是运行 Python 程序的程序。
+
+2. 什么是源代码？
+
+答：源代码是为程序所写的语句：它包括了文本文件（通常以.py 为后缀名）的文本。
+
+3. 什么是字节码？
+
+答：字节码是 Python 将程序编译后所得到的底层形式。Python 自动将字节码保存到后缀名为.pyc 的文件中。
+
+4. 什么是 PVM？
+
+答：PVM 是 Python 虚拟机，它是 Python 的运行时引擎解释编译得到的代码。
+
+5. 请列出两个 Python 标准执行模块的变体的名字。
+
+答：Psyco、Shedskin 以及 forzen binaries 是执行模块的所有变体。
+
+6. CPython、Jython 以及 IronPython 有什么不同？
+
+答: Cpython 是 Python 语言的标准实现。Jython 和 IronPython 分别是 Python 程序的 Java 和.NET 的实现；
+它们都是 Python 的编译器的替代实现。
+
+'''
