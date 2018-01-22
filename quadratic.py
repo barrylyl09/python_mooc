d512504e323ddd6d1b9acb6d687706612f73b52d
LYL Barry <barrylyl09@gmail.com>
2018-01-22 19:27:56 +0800

    quadratic

---
+++ quadratic.py
@@ -0,0 +1,19 @@
+#encoding:utf-8
+
+import math
+
+def main():
+
+    print("Let us finds the solutions to a quadratic\n")
+    a, b. c = input("Do enter the coefficients (a, b, c): \n")
+    delta = b * b - 4 * a * c
+    if delta < 0:
+        print ("\nThe equation has no real roots!")
+    elif delta == 0:
+        x = -b / (2 * a)
+        print ("\nThere is a double root at", x)
+    else:
+        print (heihei)
+
+
+main()
