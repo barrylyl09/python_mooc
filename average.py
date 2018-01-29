ed47f776377626729a94939924060c25258ea248
LYL Barry <barrylyl09@gmail.com>
2018-01-29 18:04:20 +0800

    update

--- average.py
+++ average.py
@@ -2,6 +2,9 @@
 
 import math
 
+# 求平均值
+
+# for循环求解
 def void1():
     n = input("How many numbers? ")
     sum = 0.0
@@ -10,5 +13,14 @@ def void1():
         sum = sum + x
 
     print ("\nThe average is", sum / n)
+
+# 无线循环 （while）
+def void2()
+    i = 0
+    while i <= 10:
+        print (i)
+        i = i + 1
+        
+
 
 void1()
