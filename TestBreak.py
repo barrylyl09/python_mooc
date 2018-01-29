5b332783e5580234d5906e4842846524035f6f4f
LYL Barry <barrylyl09@gmail.com>
2018-01-29 18:11:17 +0800

    update

--- TestBreak.py
+++ TestBreak.py
@@ -16,4 +16,25 @@ def test():
     print ("The sum is",sum)
 
 
+def testContinue():
+    for num in range(2,10):
+        if num % 2 == 0:
+            print ("Found an even number", num)
+            continue
+        print ("Found a number ", num)
+
+
+def testElse():
+    for n in range(2,10):
+        if x in range(2,n):
+            if n % x == 0:
+                print (n, 'equals',x,'*', n//x)
+                break
+
+        else:
+            # loop fell through without finding a factor
+            print (n,'is a prime number')
+
+
+
 test()
