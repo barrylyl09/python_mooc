6789ac8d1cc1fde8c6ebf73c9a95bfb1cad4b1e8
LYL Barry <barrylyl09@gmail.com>
2018-01-30 17:56:15 +0800

    update

--- average.py
+++ average.py
@@ -15,12 +15,22 @@ def void1():
     print ("\nThe average is", sum / n)
 
 # 无线循环 （while）
-def void2()
+def void2():
     i = 0
     while i <= 10:
         print (i)
         i = i + 1
         
 
+# 哨兵循环
+def sentry():
+    sum = 0.0
+    count = 0
+    x = input("Enter a number (negative to quit) >> ")
+    while x >= 0:
+        sum = sum + x
+        count = count + 1
+        x = input ("Enter a number (negative to quit) >>")
+    print ("\nThe average of the numbers is", sum/count)
 
 void1()
